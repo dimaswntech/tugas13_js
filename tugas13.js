39c8683b39c325f5ff05d64491691a505a8610ed
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:08:12 +0700

    Add files via upload

---
+++ tugas13.js
@@ -0,0 +1,21 @@
+// 1. Pastikan Teman Teman Sudah Menginstall Node JS
+
+// 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas13.js
+
+// 3. Buatlah Program Dengan Spesifikasi dibawah ini :
+
+// Teman teman buatlah sebuah variabel array dengan 2 buah nama buah buahan pisang , jeruk
+// Setelah itu teman teman tampilkan nilai awal array tersebut, lakukan perintah unshift dan tambahkan data  apel dan mangga seperti :
+
+
+// Hint : Jangan Lupa Console.log() setelah melakukan unshift
+
+// 4. Happy Codding :)
+
+function fruit(){
+    var buah = ['pisang', 'jeruk'];
+    console.log(buah);
+    buah.unshift('apel','mangga');
+    console.log(buah);
+}
+fruit();
